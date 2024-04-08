5064fe674dc34cb3c600114eea40859420b2d748
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2024-04-08 12:36:01 +0200

    Added the search engine.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from "./logo.jpg";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <main>
+            <Dictionary />
+          </main>
+        </header>
+      </div>
     </div>
   );
 }
