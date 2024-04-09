8204357f2275e4e73e0120ccd175a26e81db9b26
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2024-04-09 16:04:23 +0200

    Created the synonyms.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="meaning">
       <h2>{props.meaning.partOfSpeech}</h2>
-      <p>{props.meaning.definition}</p>
-      <p>{props.meaning.example}</p>
+      <p>
+        <strong>Definition: </strong>
+        {props.meaning.definition}
+      </p>
+      <p>
+        <em>Example: </em>
+        {props.meaning.example}
+      </p>
+      <Synonyms synonyms={props.meaning.definition.synonyms} />
     </div>
   );
 }
---
+++ src/Synonyms.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className="synonyms">
+        {props.meaning.synonyms.map(function (synonyms, index) {
+          return <span key={index}>{synonyms}</span>;
+        })}
+        ;
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
